1361f85e8603618283408bd13c25430f660177d6
siddharthpasad <69682536+siddharthpasad@users.noreply.github.com>
2020-08-15 19:39:13 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,88 +1,88 @@
-const Engine = Matter.Engine;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Body = Matter.Body;
-const Constraint = Matter.Constraint;
-
-var tree, stone,ground, launcherObject;
-var mango1,mango2,mango3,mango4,mango5;
-var boy,boyImg;
+var database ,dog,dog1,dog2
+var position
+//var form
+var feed,add
+var foodobject
+var Feedtime
+var Lastfeed
+//Create variables here
 
 function preload()
+
 {
-	boyImg = loadImage("Plucking_mangoes/boy.png");
+  dogimg1 = loadImage("images/dogImg.png")
+  dogimg2 = loadImage("images/dogImg1.png")
+	//load images here
 }
 
 function setup() {
-	createCanvas(1350, 600);
-
-
-	engine = Engine.create();
-	world = engine.world;
-
-	//Create the Bodies Here.
-	boy = createSprite(200,550);
-	boy.addImage(boyImg);
-	boy.scale = 0.1;
-	tree = new Tree(900,350,30,300);
-	ground = new Ground(600,600,2000,20);
-	mango1 = new Mango(900,250,15);
-	mango2 = new Mango(800,200,15);
-	mango3 = new Mango(800,280,15);
-	mango4 = new Mango(1000,250,15);
-	mango5 = new Mango(670,300,15);
-	stone = new Stone(150,550,15);
-	boyShot = new Shot(stone.body,{x:150,y:500});
-	Engine.run(engine);
-  
-}
-
-
-function draw() {
-  rectMode(CENTER);
-  
-  Engine.update(engine);
-
-  background("lightBlue");
-  tree.display();
-  ground.display();
-  mango1.display();
-  mango2.display();
-  mango3.display();
-  mango4.display();
-  mango5.display();
-  stone.display();
-  boyShot.display();
-  detectCollision(stone,mango1);
-  detectCollision(stone,mango2);
-  detectCollision(stone,mango3);
-  detectCollision(stone,mango4);
-  detectCollision(stone,mango5);
-  drawSprites();
+	createCanvas(1000, 500);
+  database = firebase.database();
+  console.log(database);
  
+  foodobject=new Food()
+  dog = createSprite(550,250,10,10);
+  dog.addImage(dogimg1)
+  dog.scale=0.2
+ 
+  var dogo = database.ref('Food');
+  dogo.on("value", readPosition, showError);
+  feed = createButton("FEED DRAGO")
+  feed.position(500,15)
+  feed.mousePressed(FeedDog)
+  add = createButton("ADD FOOD")
+  add.position(400,15)
+  add.mousePressed(AddFood)
+
+} 
+
+function draw(){
+ background(46,139,87);
+
+ foodobject.display()
+ 
+ drawSprites();
+  
+ fill(255,255,254);
+ textSize(15);
+
+drawSprites();
+}
+function readPosition(data){
+  position = data.val();
+  foodobject.updateFoodStock(position)
 }
 
-
-function mouseDragged(){
-    Matter.Body.setPosition(stone.body,{x:mouseX,y:mouseY});
+function showError(){
+  console.log("Error in writing to the database");
 }
 
-function mouseReleased(){
-    boyShot.fly();
+function writePosition(nazo){
+  if(nazo>0){
+    nazo=nazo-1
+  }
+  else{
+    nazo=0
+  }
+  database.ref('/').set({
+    'Food': nazo
+  })
+
 }
-function detectCollision(lstone,lmango){
-	mangoBodyPosition=lmango.body.position
-	stoneBodyPosition=lstone.body.position
-	var distance=dist(stoneBodyPosition.x,stoneBodyPosition.y,mangoBodyPosition.x,mangoBodyPosition.y)
-	if(distance<=lmango.r+lstone.r){
-		Matter.Body.setStatic(lmango.body,false);
-	}
+function AddFood(){
+position++
+database.ref('/').update({
+  Food:position
 }
 
-function keyPressed(){
+)
+}
+function FeedDog(){
 
-	if(keyCode === 32){
-		Matter.Body.setPosition(stone.body,{x:150,y:550})
-		boyShot.attach(stone.body);
-	}
+dog.addImage(dogimg2)
+foodobject.updateFoodStock(foodobject.getFoodStock()-1)
+ database.ref('/').update({
+   Food:foodobject.getFoodStock(),
+   FeedTime:hour ()
+ })
 }
